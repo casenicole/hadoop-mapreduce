14b4b5ee43a13a6be69bfd823cbf35d6e18192f4
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-11-05 10:41:44 -0600

    Update DocumentParsingReducer.java
    I don't know if you've gotten a more updated version of this Reducer, but here was my idea on how to perhaps fix it (although you may have already done this).

--- DocumentParsingReducer.java
+++ DocumentParsingReducer.java
@@ -1,16 +1,20 @@
 import java.io.IOException;
 
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 // Create a Reducer class and implement the reduce function.
 // Input Key,Value: Text, Article
 // Output Key,Value: Text, Article
+public class DocumentParsingReducer extends Reducer<Text, Article , Text, Article> {
 
-// This Reducer does nothing but passing on the key values through the context object. 
-// Simply iterate through the values and write the key,value pairs to the context object.
+  // This Reducer does nothing but passing on the key values through the context object. 
+  // Simply iterate through the values and write the key,value pairs to the context object.
+  public void reduce(Text key, Iterable <Article> values, Context context)
+	  throws IOException, InterruptedException {
+	  while (values.iterator().hasNext()) {
+		  Article value = values.iterator().next();
+		  context.write(key, value);
+	  }
+  }
+}
