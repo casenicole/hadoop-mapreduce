50bac2165606c8862f755d368cb1b854582a30dc
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-11-05 15:26:29 -0600

    Update OverallStatsMapper.java
    Tentative implementation

--- OverallStatsMapper.java
+++ OverallStatsMapper.java
@@ -11,6 +11,17 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 //Create a Mapper class and implement the map function.
 //Input Key,Value: LongWritable, Text
 //Output Key,Value: LongWritable, IntWritable
-
+public class OverallStatsMapper extends Mapper<LongWritable, Text, LongWritable, IntWritable>{
 //In the map function, you need to split the content of each line and get the body column.
 //Write to the context object the smae input key and the length (character count) of the text of the body column as the value.
+	
+	
+	public void map(Iterable<LongWritable> lines, Iterable<Text> contents, Context context) throws IOException, InterruptedException{
+		while(lines.iterator().hasNext()){
+		String[] line = lines.iterator().next().toString().split("\t");	
+		LongWritable key = new LongWritable(Long.parseLong(line[4]));
+		IntWritable value = new IntWritable(line[4].length());
+	
+		context.write(key, value);
+		}
+	
