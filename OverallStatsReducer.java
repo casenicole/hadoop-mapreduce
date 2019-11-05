d1a179a107f8f44b64070504ce44762ba642c481
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-11-05 15:41:52 -0600

    Update OverallStatsReducer.java
    Tentative implementation.

--- OverallStatsReducer.java
+++ OverallStatsReducer.java
@@ -10,6 +10,21 @@ import org.apache.hadoop.mapreduce.Reducer;
 //Input Key,Value: LongWritable, IntWritable
 //Output Key,Value: Text, DoubleWritable
 
+public class OverallStatsReducer extends Reducer<LongWritable, IntWritable, Text, DoubleWritable>{
 //This Reducer should follow the example we learned in class to compute the count and average values.
 //The output should look as clarified in the instructions document.
+int doc_count = 0;
+int body_word_count = 0;
+int average_body_word_count = 0;
 
+public void reduce(LongWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+	while(values.iterator().hasNext()){
+		doc_count++;
+		body_word_count += values.iterator().next().get();
+	}
+	average_body_word_count = body_word_count / doc_count;
+	
+	context.write(new Text("Count"), new DoubleWritable(doc_count));
+	context.write(new Text("Average"), new DoubleWritable(average_body_word_count));
+}
+}
