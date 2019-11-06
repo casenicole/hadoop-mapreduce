53e0cafd429152cc0e2a1ef930f7a6485b26445b
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-11-06 15:44:52 -0600

    Update CategoryOverallStatsReducer.java
    Irritatingly only passes Dummy Value to max and min categories.

--- CategoryOverallStatsReducer.java
+++ CategoryOverallStatsReducer.java
@@ -12,38 +12,35 @@ public class CategoryOverallStatsReducer extends Reducer<Text, Text, Text, Text>
 	int maxWordCount = Integer.MAX_VALUE;
 	int minWordCount = Integer.MIN_VALUE;
 	int average_docs_per_category = 0;
-	String maxCategory;
-	String minCategory;
+	private static String maxCategory = "Dummy Value";
+	private static String minCategory = "Dummy Value";
 	
 	
-	public void reduce(Iterable<Text> categories, Iterable<Text> averageBodySizeAndDocCounts, Context context) throws IOException, InterruptedException{
+	public void reduce(Text category, Iterable<Text> averageBodySizeAndDocCounts, Context context) throws IOException, InterruptedException{
 		
 		
 		int doc_count = 0;
-		while(categories.iterator().hasNext()){
-			String[] accountedForCategories = new String[20];
-			for(int i = 0; i <= 20; i++){
-				if(accountedForCategories[i] != categories.iterator().next().toString()){
-					accountedForCategories[i] = categories.iterator().next().toString();
-				} else{
-					String[] average_body_size_and_doc_count = averageBodySizeAndDocCounts.iterator().next().toString().split(",");
-					doc_count += Integer.parseInt(average_body_size_and_doc_count[1]);
-					int bodyWordCount = Integer.parseInt(average_body_size_and_doc_count[0]);
+		while(averageBodySizeAndDocCounts.iterator().hasNext()){
+			
+			
+				String[] average_body_size_and_doc_count = averageBodySizeAndDocCounts.iterator().next().toString().split(",");
+				doc_count += Integer.parseInt(average_body_size_and_doc_count[1]);
+				int bodyWordCount = Integer.parseInt(average_body_size_and_doc_count[0]);
 					
-					if(bodyWordCount > maxWordCount){
-						maxWordCount = bodyWordCount;
-						maxCategory = accountedForCategories[i];
+				if(bodyWordCount > maxWordCount){
+					maxWordCount = bodyWordCount;
+					maxCategory = category.toString();
 					} else if (bodyWordCount < minWordCount){
 						minWordCount = bodyWordCount;
-						minCategory = accountedForCategories[i];
+						minCategory = category.toString();
 					} else {
 						continue;
 					}
 				}
-			}
+			
  		average_docs_per_category = doc_count / 20;
 		}
-	}
+	
 	
 	public void cleanup(Context context) throws IOException, InterruptedException{
 		context.write(new Text("Average Document Per Category"), new Text(Integer.toString(average_docs_per_category)));
