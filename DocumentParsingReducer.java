19f62ca5ee2d4ccaa89216cbf51a07d4201233ab
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-11-06 15:47:22 -0600

    Update DocumentParsingReducer.java

--- DocumentParsingReducer.java
+++ DocumentParsingReducer.java
@@ -8,13 +8,13 @@ import org.apache.hadoop.mapreduce.Reducer;
 // Output Key,Value: Text, Article
 public class DocumentParsingReducer extends Reducer<Text, Article , Text, Article> {
 
-  // This Reducer does nothing but passing on the key values through the context object. 
-  // Simply iterate through the values and write the key,value pairs to the context object.
-  public void reduce(Text key, Iterable <Article> values, Context context)
-	  throws IOException, InterruptedException {
-	  while (values.iterator().hasNext()) {
-		  Article value = values.iterator().next();
-		  context.write(key, value);
-	  }
-  }
+// This Reducer does nothing but passing on the key values through the context object. 
+// Simply iterate through the values and write the key,value pairs to the context object.
+public void reduce(Text key, Iterable <Article> values, Context context)
+	throws IOException, InterruptedException {
+	while (values.iterator().hasNext()) {
+		Article value = values.iterator().next();
+		context.write(key, value);
+	}
+}
 }
