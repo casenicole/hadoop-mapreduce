37d333062b54337c3998be2f956caff69228a55d
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-11-06 15:46:06 -0600

    Update CategoryStatsReducer.java

--- CategoryStatsReducer.java
+++ CategoryStatsReducer.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -8,24 +10,35 @@ import org.apache.hadoop.mapreduce.Reducer;
 //Input Key,Value: Text, IntWritable
 //Output Key,Value: Text, Text
 public class CategoryStatsReducer extends Reducer<Text, IntWritable , Text, Text> {
-
+	private static List<String> cat = new ArrayList<String>();
+	private static List<String> bodySizeDocCount = new ArrayList<String>();
+	
 //This Reducer should compute the count and average body size for each category. 
 //Write to the context object the category as the key and "AverageBodySize,DocCount" as the value (this is one string with the two values separated by a comma). 
-	public void reduce(Iterable<Text> categories, Iterable<IntWritable> bodySize, Context context)
+	public void reduce(Text category, Iterable<IntWritable> bodySize, Context context)
 	throws IOException, InterruptedException{
 		int document_count = 0;
 		int body_size = 0;
-		Text[] accountedForCategories = new Text[20];
-		for(int i = 0; i < 20; i++){
-			accountedForCategories[i] = categories.iterator().next();
-			while(categories.iterator().next() == accountedForCategories[i]){
-				document_count++;
-				body_size += bodySize.iterator().next().get();
-			}
-			Text key = accountedForCategories[i];
-			int average_body_size = body_size / document_count;
-			Text value = new Text(average_body_size + "," + document_count);
-			context.write(key, value);
+		while(bodySize.iterator().hasNext()){
+			document_count++;
+			body_size += bodySize.iterator().next().get();
+		}
+		if(!cat.contains(category.toString())){
+			cat.add(category.toString());
+		}
+	
+		int average_body_size = body_size / document_count;
+		
+		if(!bodySizeDocCount.contains(average_body_size))
+		{
+			bodySizeDocCount.add(average_body_size + "," + document_count);
+		}	
+		
+		}
+	
+	public void cleanup(Context context) throws IOException, InterruptedException{
+		for(int i = 0; i < cat.size(); i++){
+		context.write(new Text(cat.get(i)), new Text(bodySizeDocCount.get(i)));
 		}
 	}
-}
+	}
