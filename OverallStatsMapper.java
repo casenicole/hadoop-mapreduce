204c2f5d051d67ef3f1276dcccc83404e5dbe5c3
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-11-06 15:47:53 -0600

    Update OverallStatsMapper.java

--- OverallStatsMapper.java
+++ OverallStatsMapper.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
 
-import org.apache.hadoop.io.DoubleWritable;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 //Create a Mapper class and implement the map function.
 //Input Key,Value: LongWritable, Text
@@ -16,11 +14,12 @@ public class OverallStatsMapper extends Mapper<LongWritable, Text, LongWritable,
 //Write to the context object the smae input key and the length (character count) of the text of the body column as the value.
 	
 	
-	public void map(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
-		while(values.iterator().hasNext()){
-		String[] line = values.iterator().next().toString().split("\t");	
-		IntWritable value = new IntWritable(line[4].length());
-		context.write(key, value);
-		}
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+		
+		String[] line = value.toString().split("\t");	
+		IntWritable body_length = new IntWritable(line[4].length());
+	
+		context.write(key, body_length);
+		
 	}
 }
